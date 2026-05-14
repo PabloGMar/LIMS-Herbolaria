f62478b5015089a3eec090873b4bcbbcd78cdb18
PabloGMar <pagalmar@gmail.com>
2026-05-14 13:03:00 -0600

    Update ConexionSupabase.js

--- Backup/ConexionSupabase.js
+++ Backup/ConexionSupabase.js
@@ -4,7 +4,7 @@
  */
 
 const SB_URL = "https://lbemzbaytsrtbmkskder.supabase.co";
-const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZW16YmF5dHNydGJta3NrZGVyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzQwMjM2MywiZXhwIjoyMDkyOTc4MzYzfQ.GTwONBiJFNsU7gEYSonT-EfcI95E0Swq3iaRGDGD8u0";
+const SB_KEY = "...";
 
 /**
  * Función Maestra para Insertar (POST)
